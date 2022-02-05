a2cce354a5a2079548deb33bd4dc2bc7273fe9a9
geet <geeti_hashemi@yahoo.com>
2022-02-05 11:17:39 -0800

    A name in a box

--- box_it_script/boxit.js
+++ box_it_script/boxit.js
@@ -4,19 +4,30 @@ for (let i = 2; i < process.argv.length; i++) {
   names.push(process.argv[i]);
 }
 
-// No name print empty box or list names
+// No name print empty box or a name in a box
 function boxIt(names) {
   const TopLeftCorner = "┏";
   const TopRightCorner = "┓";
   const BottomLeftCorner = "┗";
   const BottomRightCorner = "┛";
+  const MiddleLine = "━";
+  const SideLine = "┃";
+  const LeftSideT = "┣";
+  const RightSideT = "┫";
   // Print an empty box
   if (names.length === 0) {
     console.log(TopLeftCorner + TopRightCorner);
     console.log(BottomLeftCorner + BottomRightCorner);
   } else {
     names.forEach((name) => {
-      console.log(name);
+      // A name in a box
+      console.log(
+        TopLeftCorner + MiddleLine.repeat(name.length) + TopRightCorner
+      );
+      console.log(SideLine + name + SideLine);
+      console.log(
+        BottomLeftCorner + MiddleLine.repeat(name.length) + BottomRightCorner
+      );
     });
   }
 }
