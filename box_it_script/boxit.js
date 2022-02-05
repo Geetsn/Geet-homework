755a37e2655c796f3d171b3dcb1457e5131ffcdd
geet <geeti_hashemi@yahoo.com>
2022-02-05 10:33:50 -0800

    No name print empty box or list names

--- box_it_script/boxit.js
+++ box_it_script/boxit.js
@@ -4,11 +4,21 @@ for (let i = 2; i < process.argv.length; i++) {
   names.push(process.argv[i]);
 }
 
-// List names without boxes
+// No name print empty box or list names
 function boxIt(names) {
-  names.forEach((name) => {
-    console.log(name);
-  });
+  const TopLeftCorner = "┏";
+  const TopRightCorner = "┓";
+  const BottomLeftCorner = "┗";
+  const BottomRightCorner = "┛";
+  // Print an empty box
+  if (names.length === 0) {
+    console.log(TopLeftCorner + TopRightCorner);
+    console.log(BottomLeftCorner + BottomRightCorner);
+  } else {
+    names.forEach((name) => {
+      console.log(name);
+    });
+  }
 }
 
 boxIt(names);
