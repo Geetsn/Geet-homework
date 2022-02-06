7888418c83ea14d99c5bc787860e2ffd04da65c7
geet <geeti_hashemi@yahoo.com>
2022-02-05 16:59:54 -0800

    Create Turtle class with forward method

---
+++ turtle_graphics/turtle.js
@@ -0,0 +1,36 @@
+class Turtle {
+  constructor(x, y) {
+    this.currPoint = [x, y];
+    this.visitedPoints = [[this.currPoint[0], this.currPoint[1]]]; // store the current point as visited
+
+    this.currDirection = 0; // 0 right, 1 down, 2 left, 3 up
+  }
+  forward(steps) {
+    for (let step = 0; step < steps; step++) {
+      switch (this.currDirection) {
+        case 0:
+          this.currPoint[0] += 1; // Increase x axis value
+          this.visitedPoints.push([this.currPoint[0], this.currPoint[1]]);
+          break;
+        case 1:
+          this.currPoint[1] += 1; // Increase y axis value
+          this.visitedPoints.push([this.currPoint[0], this.currPoint[1]]);
+          break;
+        case 2:
+          this.currPoint[0] -= 1; // Decrease x axis value
+          this.visitedPoints.push([this.currPoint[0], this.currPoint[1]]);
+          break;
+        case 3:
+          this.currPoint[1] -= 1; // Decrease y axis value
+          this.visitedPoints.push([this.currPoint[0], this.currPoint[1]]);
+          break;
+        default:
+          break;
+      }
+    }
+    return this; // For chaining
+  }
+}
+
+const myTurtle = new Turtle(0, 0).forward(3);
+console.log(myTurtle);
