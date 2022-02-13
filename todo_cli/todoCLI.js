d53794696f20f971f9fdaeef6ab0350fbd279407
geet <geeti_hashemi@yahoo.com>
2022-02-12 19:21:14 -0800

    Create View action, refactor Menu

--- todo_cli/todoCLI.js
+++ todo_cli/todoCLI.js
@@ -1,23 +1,41 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "> "
-})
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
+});
 
-const theMenu = "(v)View . (n) New . (cX) Complete . (dX) Delete . (q) Quit"
+function menuPrompt() {
+  const theMenu = "(v)View . (n) New . (cX) Complete . (dX) Delete . (q) Quit";
+  console.log(theMenu);
+  rl.prompt();
+}
+
+function showList() {
+  theList.forEach((entry, index) => {
+    let status = entry[0] === true ? "✔" : " ";
+    console.log(`${index} [${status}] ${entry[1]}`);
+  });
+}
 
 console.log("Welcome to Todo CLI!\n--------------");
-console.log(theMenu);
-rl.prompt();
+menuPrompt();
 
 rl.on("line", (action) => {
- 
   if (action === "q") {
     rl.close();
-  } else {
-    console.log(theMenu);
-    rl.prompt();
+    return;
+  } else if (action === "v") {
+    showList();
   }
+  menuPrompt();
 });
+
+const theList = [
+  [true, "Take out the trash"],
+  [true, "Buy toothpaste"],
+  [false, "Buy Snickerdoodles"],
+  [false, "Fix the climate"],
+  [false, "Find a cure for aging"],
+];
