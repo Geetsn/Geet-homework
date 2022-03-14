dd82e71c2584a06f43db3d712af1c2ad84eca285
geet <geeti_hashemi@yahoo.com>
2022-03-13 23:50:03 -0700

    Create Posts contoller and views with all RESTful routes

---
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+# postsController
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find params[:id]
+  rescue StandardError => e
+    @error_message = e.message
+  end
+
+  def destroy
+    @post = Post.find params[:id]
+    @post.destroy
+    redirect_to posts_path, { notice: 'Post deleted successfully', status: 303 }
+  rescue StandardError => e
+    redirect_to posts_path, alert: e.message
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new params.require(:post).permit(:title, :body)
+    if @post.save
+      redirect_to post_path(@post), { status: 303, notice: 'Post created successfully' }
+    else
+      render :new, status: 303
+    end
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  rescue StandardError => e
+    redirect_to posts_path, alert: e.message
+  end
+
+  def update
+    @post = Post.find params[:id]
+    puts @post
+    if @post.update params.require(:post).permit(:title, :body)
+      puts @post
+      redirect_to post_path(@post), { status: 303, notice: 'Post updated successfully' }
+    else
+      render :edit, status: 303
+    end
+  rescue StandardError => e
+    redirect_to posts_path, alert: e.message
+  end
+end
