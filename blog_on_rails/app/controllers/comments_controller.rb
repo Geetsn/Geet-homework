7eb8eea13d211c7f0f6f1550c1dd0c5c4058416c
geet <geeti_hashemi@yahoo.com>
2022-03-24 22:16:12 -0700

    Add Authorization

--- blog_on_rails/app/controllers/comments_controller.rb
+++ blog_on_rails/app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
   before_action :authenticated_user!
+  before_action :find_comment, only: [:destroy]
+  before_action :authorized_user!, only: [:destroy]
+
   def create
     @post = Post.find params[:post_id]
     @comment = Comment.new params.require(:comment).permit(:body)
@@ -11,16 +14,24 @@ class CommentsController < ApplicationController
       @comments = @post.comments.order(created_at: :desc)
       render 'posts/show', status: 303
     end
-  rescue => e
-    redirect_to root_path, {alert: e.message, status: 303}
+  rescue StandardError => e
+    redirect_to root_path, { alert: e.message, status: 303 }
   end
 
   def destroy
-    @comment = Comment.find params[:id]
     @comment.destroy
-    # @post = Post.find params[:post_id]
     redirect_to post_path(@comment.post), status: 303
-  rescue => e
-    redirect_to root_path, {alert: e.message, status: 303}
+  rescue StandardError => e
+    redirect_to root_path, { alert: e.message, status: 303 }
+  end
+
+  private
+
+  def find_comment
+    @comment = Comment.find params[:id]
+  end
+
+  def authorized_user!
+    redirect_to post_path(@comment.post), { status: 303, alert: 'Not authorized' } unless can?(:destroy, @comment)
   end
 end
--- blog_on_rails/db/seeds.rb
+++ blog_on_rails/db/seeds.rb
@@ -6,20 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# First destrou all records from table comments due to FK constraint.
-# Comments.destroy_all
-
-# Destroy all records from table posts.
+# Destroy all records from table comments, posts and users.
+Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
 # Reset the primary key sequence to 1.
 ActiveRecord::Base.connection.reset_pk_sequence!(:posts)
-# ActiveRecord::Base.connection.reset_pk_sequence!(:comments)
+ActiveRecord::Base.connection.reset_pk_sequence!(:comments)
 ActiveRecord::Base.connection.reset_pk_sequence!(:users)
 
-user = User.create name: "max", email: "m@example.com", password: "111"
-if user.persisted?
+PASSWORD = '123'
+User.create(
+  name: 'Admin',
+  email: 'admin@user.com',
+  password: PASSWORD,
+  is_admin: true
+)
+
+5.times do |n|
+  User.create(
+    name: Faker::Name.first_name,
+    email: "user#{n + 1}@user.com",
+    password: PASSWORD
+  )
+end
+
+users = User.all.offset 1
+puts Cowsay.say("Generated #{users.count} users using Faker.", :tux)
 
 # Bulk insert of 50 fake posts.
 Post.insert_all(
@@ -27,12 +41,12 @@ Post.insert_all(
     {
       title: Faker::Hacker.say_something_smart,
       body: Faker::ChuckNorris.fact,
-      user_id: user.id,
-      created_at: Faker::Time.backward(days:365),
-      updated_at: DateTime.now()
+      user_id: users.sample.id,
+      created_at: Faker::Time.backward(days: 365),
+      updated_at: DateTime.now
     }
   end
 )
-end
+
 # Show how many fake posts are in the table posts.
 puts Cowsay.say("Generated #{Post.count} posts using Faker.", :frogs)
